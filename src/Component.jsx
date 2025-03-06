61bfc7da8aae21f36209dcd119ac579e83a3d3e3
mpatel184 <mvekariya398@gmail.com>
2025-03-07 00:20:22 +0530

    final

--- src/Component.jsx
+++ src/Component.jsx
@@ -16,13 +16,16 @@ export default function Component(){
         <div style={{position:"absolute",top:"50px",left:"30px"}}>
             <h1>Full Name Display</h1>
             <div className="name-field">
+                <form>
                 <label htmlFor="first-name">First Name:</label>
-                <input type="text" name="first-name" id="first_name" onChange={(e) => setFirstName(e.target.value)}/>
+                <input type="text" name="first-name" id="first_name" onChange={(e) => setFirstName(e.target.value)} required/>
                 <br/>
                 <label htmlFor="last-name">Last Name:</label>
-                <input type="text" name="last-name" id="last" onChange={(e) => setLastName(e.target.value)}/>
+                <input type="text" name="last-name" id="last" onChange={(e) => setLastName(e.target.value)} required/>
                 <br/>
                 <button onClick={displayName} style={{border:"1px solid black"}}>Submit</button>
+                </form>
+
                 <h3>{fullname}</h3>
             </div>
         </div>
