f62c223ba07e56902ab4921b3e98f4c45f3744d2
mpatel184 <mvekariya398@gmail.com>
2025-03-07 00:14:31 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Component from "./Component"
+
+function App() {
+  
+
+  return (
+    <>
+      <Component />
+    </>
+  )
+}
+
+export default App
---
+++ src/Component.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export default function Component(){
+
+    const [firstname,setFirstName] = useState("")
+    const [lastname,setLastName] = useState("")
+    const [fullname,setFullName] = useState("")
+
+    const displayName = () => {
+        setFullName(`${firstname} ${lastname}`)
+        setFirstName("")
+        setLastName("")
+    }
+
+    return(
+        <div style={{position:"absolute",top:"50px",left:"30px"}}>
+            <h1>Full Name Display</h1>
+            <div className="name-field">
+                <label htmlFor="first-name">First Name:</label>
+                <input type="text" name="first-name" id="first_name" onChange={(e) => setFirstName(e.target.value)}/>
+                <br/>
+                <label htmlFor="last-name">Last Name:</label>
+                <input type="text" name="last-name" id="last" onChange={(e) => setLastName(e.target.value)}/>
+                <br/>
+                <button onClick={displayName} style={{border:"1px solid black"}}>Submit</button>
+                <h3>{fullname}</h3>
+            </div>
+        </div>
+    )
+}
